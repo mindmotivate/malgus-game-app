e95ad8e34e88a796148eda24ce07ecb02b56da5f
mindmotivate <130941970+mindmotivate@users.noreply.github.com>
2024-07-22 20:46:54 +0000

    Deploying to gh-pages from @ mindmotivate/malgus-game-app@61e5a42f3af33264c9901161390c0f1ba17914fa 🚀

---
+++ precache-manifest.c698e581452f4e9e8582b0c3a7dcb1b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ad12ba39624d364680fb857ff6d04a",
+    "url": "/malgus-game-app/index.html"
+  },
+  {
+    "revision": "cdef9f21b46cb8edaea1",
+    "url": "/malgus-game-app/static/css/main.cb2b127c.chunk.css"
+  },
+  {
+    "revision": "16abd7bec715f8e6fc95",
+    "url": "/malgus-game-app/static/js/2.3908a07a.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/malgus-game-app/static/js/2.3908a07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdef9f21b46cb8edaea1",
+    "url": "/malgus-game-app/static/js/main.ec5a1b5a.chunk.js"
+  },
+  {
+    "revision": "ab27f54f20ed46180900",
+    "url": "/malgus-game-app/static/js/runtime-main.ee7a8850.js"
+  },
+  {
+    "revision": "b97f964e1155eb6608d601b44c61f75e",
+    "url": "/malgus-game-app/static/media/background.b97f964e.jpg"
+  }
+]);
